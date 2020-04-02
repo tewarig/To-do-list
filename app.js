4bb83678a7e61c7d818b6790fe3d4ee7709c17af
tewarig <gauravtewari@techie.com>
2020-04-02 19:21:56 +0530

    yo

--- app.js
+++ app.js
@@ -140,11 +140,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-app.listen(port, function() {
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
